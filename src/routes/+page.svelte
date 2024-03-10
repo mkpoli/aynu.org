3e1d49c82975068c8367668829b59a8d08c80916
mkpoli <git@mkpo.li>
2024-03-10 21:59:06 +0900

    Add basic content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<h1>Aynu.org Mosem</h1>
+
+	<h2>Cikarkoranpe</h2>
+
+	<ul>
+		<li>
+			<a href="https://wiki.aynu.org/"> Aynuwiki </a>
+		</li>
+		<li>
+			<a href="https://site.aynu.org/"> Aynuitak Inuypa Ukosammaani Sait </a>
+		</li>
+		<li>
+			<a href="https://dict.aynu.org/"> Aynuitak-Eonnekunnep </a>
+		</li>
+	</ul>
+
+	<h2>Uwekarpare</h2>
+
+	<ul>
+		<li>
+			<a href="https://discord.aynu.org/"> Discord </a>
+		</li>
+	</ul>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		min-height: 100svh;
+	}
+</style>
