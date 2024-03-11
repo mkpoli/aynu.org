40fcdeba7da6ab6e403ea33776cb98c40f4b8cc1
mkpoli <git@mkpo.li>
2024-03-11 13:04:12 +0900

    Add Lucas and Karumo

--- src/lib/sections/SectionMembers.svelte
+++ src/lib/sections/SectionMembers.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
-	import Hachia from '$assets/images/Hachia.png';
-	import Mkpoli from '$assets/images/Mkpoli.jpg';
+	import HachiaPNG from '$assets/images/avatars/png/Hachia.png';
+	import MkpoliPNG from '$assets/images/avatars/png/Mkpoli.png';
+	import LucasPNG from '$assets/images/avatars/png/Lucas.png';
+	import KarumoPNG from '$assets/images/avatars/png/Karumo.png';
+	import HachiaAVIF from '$assets/images/avatars/avif/Hachia.avif';
+	import MkpoliAVIF from '$assets/images/avatars/avif/Mkpoli.avif';
+	import LucasAVIF from '$assets/images/avatars/avif/Lucas.avif';
+	import KarumoAVIF from '$assets/images/avatars/avif/Karumo.avif';
 </script>
 
 <section>
@@ -9,14 +15,42 @@
 	<div class="members">
 		<div class="member-card">
 			<a href="https://twitter.com/hachia_go" target="_blank">
-				<img src={Hachia} alt="Hachia" />
+				<picture>
+					<source srcset={HachiaAVIF} type="image/avif" />
+					<source srcset={HachiaPNG} type="image/png" />
+					<img src={HachiaPNG} alt="Hachia" />
+				</picture>
 				<h3>はちあ</h3>
 			</a>
 		</div>
 		<div class="member-card">
 			<a href="https://twitter.com/mkpoli" target="_blank">
-				<img src={Mkpoli} alt="Hachia" />
+				<picture>
+					<source srcset={MkpoliAVIF} type="image/avif" />
+					<source srcset={MkpoliPNG} type="image/png" />
+					<img src={MkpoliPNG} alt="Mkpoli" />
+				</picture>
 				<h3>まくぽり</h3>
+			</a>
+		</div>
+		<div class="member-card">
+			<a href="https://twitter.com/Lucas_Yoyogi" target="_blank">
+				<picture>
+					<source srcset={LucasAVIF} type="image/avif" />
+					<source srcset={LucasPNG} type="image/png" />
+					<img src={LucasPNG} alt="Lucas" />
+				</picture>
+				<h3>代々木 ルーカス</h3>
+			</a>
+		</div>
+		<div class="member-card">
+			<a href="https://twitter.com/ranekrm" target="_blank">
+				<picture>
+					<source srcset={KarumoAVIF} type="image/avif" />
+					<source srcset={KarumoPNG} type="image/png" />
+					<img src={KarumoPNG} alt="Karumo" />
+				</picture>
+				<h3>かるも</h3>
 			</a>
 		</div>
 		<p>ne wa na an na…</p>
